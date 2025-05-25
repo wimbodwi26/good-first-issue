0fcc790e3be95677af96bbb4105f1bf8c2ffec80
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2025-05-25 14:59:14 -0400

    Header title fix

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import Head from 'next/head';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Good First Issues</title>
+        <meta name="description" content="Find beginner-friendly open source issues" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
